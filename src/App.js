00c706ee6ce143b9a5c5ccce38d53a45ef376439
Jeff Berlin <jeff@jbrally.com>
2017-04-27 17:07:01 -0400

    Button is console logging properly
    Button is console logging properly, but is not appended yet.

--- src/App.js
+++ src/App.js
@@ -4,32 +4,36 @@ import activitiesArray from './activities';
 
 class App extends Component {
 
-  // function randomActivity() {
-  //   const random = activitiesArray[Math.floor(Math.random() * activitiesArray.length)]
+  randomActivity = () => {
 
-  //   ReactDOM.render(
-  //     random,
-  //     document.getElementById('activity')
-  //   )
-  // }
+    var random = activitiesArray[Math.floor(Math.random() * activitiesArray.length)]
+
+    console.log(random);
+  }
   
   render() {
     return (
       <div className="App">
+
         <div className="App-header">
           <h1>Random Activity Selector</h1>
         </div>
+
         <div className="App-intro">
           <h3>Oh, what to do?</h3>
         </div>
+
         <div className="App-body">
           <p>Click here to get an activity to try:</p>
-          <input className="App-button" type="button" value="Activity"></input>
+          <input className="App-button" type="button" value="Activity" onClick={this.randomActivity}></input>
         </div>
-        <div className="activity"></div>
+
+        <div id="activity"></div>
+
       </div>
     );
   }
+
 }
 
 

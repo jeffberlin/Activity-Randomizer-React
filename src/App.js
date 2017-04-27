8fad8618529ff6f6894737698b7e582f9619fdb8
Jeff Berlin <jeff@jbrally.com>
2017-04-27 15:42:37 -0400

    Layout is almost complete
    Layout is almost complete. Arrays file is created, working on getting
    the button and its function to work.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import activitiesArray from './activities';
 
 class App extends Component {
+
+  // function randomActivity() {
+  //   const random = activitiesArray[Math.floor(Math.random() * activitiesArray.length)]
+
+  //   ReactDOM.render(
+  //     random,
+  //     document.getElementById('activity')
+  //   )
+  // }
+  
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h1>Random Activity Selector</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <h3>Oh, what to do?</h3>
+        </div>
+        <div className="App-body">
+          <p>Click here to get an activity to try:</p>
+          <input className="App-button" type="button" value="Activity"></input>
+        </div>
+        <div className="activity"></div>
       </div>
     );
   }
 }
+
 
 export default App;

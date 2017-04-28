cdd479c055b19820fdd56ae3fb25974b6db3610b
Jeff Berlin <jeff@jbrally.com>
2017-04-28 16:11:56 -0400

    Foundation is working
    Foundation is working and everything is spaced properly.

--- src/App.js
+++ src/App.js
@@ -9,39 +9,55 @@ class App extends Component {
     var random = activitiesArray[Math.floor(Math.random() * activitiesArray.length)]
 
     document.getElementById("activity").innerHTML = random;
-    console.log(random);
   }
   
   render() {
     return (
       <div className="App">
 
-        <div className="App-header">
-          <h1>Random Activity Selector</h1>
+        <div className="row">
+          <div className="large-6 large-centered columns" id="header">
+            <h1>Random Activity Selector</h1>
+          </div>
         </div>
 
-        <div className="App-intro">
-          <h3>Oh, what to do?</h3>
+        <div className="row">
+          <div className="large-6 large-centered columns" id="callout">
+            <h3>Oh, what to do?</h3>
+          </div>
         </div>
 
-        <div className="App-body">
-          <p>Click here to get an activity to try:</p>
-          <input className="App-button" type="button" value="Activity" onClick={this.randomActivity}></input>
-        </div>
+          <div className="row">
+            <div className="medium-6 medium-centered columns" id="clickIt">
+              <span>Click here to get an activity to try:</span>
+              <input className="App-button" type="button" value="Activity" onClick={this.randomActivity}></input>
+            </div>
+          </div>
 
+          <div className="row">
+            <div className="medium-7 medium-centered columns" id="result">
+              <h5 id="activity"></h5>
+            </div>
+          </div>
 
-        <div className="activityArea">
-          <h5 id="activity"></h5>
-        </div>
-
-        <div className="App-images">
-          <img src={require('./images/hike.jpg')} alt="hiking"/>
-          <img src={require('./images/bball.jpeg')} alt="basketball"/>
-          <img src={require('./images/concert.jpg')} alt="concert"/>
-          <img src={require('./images/drag.jpg')} alt="drag strip"/>
-        </div>
+          <div className="row">
+            <div className="small-3 columns">
+              <img src={require('./images/hike.jpg')} alt="hiking"/>
+            </div>
+            <div className="small-3 columns">
+              <img src={require('./images/bball.jpeg')} alt="basketball"/>
+            </div>
+            <div className="small-3 columns">
+              <img src={require('./images/concert.jpg')} alt="concert"/>
+            </div>
+            <div className="small-3 columns">
+              <img src={require('./images/drag.jpg')} alt="drag strip"/>
+            </div>
+            
+          </div>
 
       </div>
+
     );
   }
 
